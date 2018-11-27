4cf7cc0625431c080ff7496f1398eb215343f5eb
Karina Amador <akarina.at@gmail.com>
2018-11-27 02:59:57 -0600

    Starting setting server

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-'use strict'
+'use strict';
+
+// var express = require('express');
+
+// const pg = require('pg');
+// const fs = require('fs');
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+const app = express();
+var routes = require('index');
+var server = app.listen(app.get('PORT'), function (){
+  console.log('app started');
+});
+// var routes = require('')
+// const conString = '';
+// const client = new pg.Client(conString);
+// client.connect();
+// client.on('error', err => {
+//   console.error(err);
+// });
+
+// app.use(express.json());
+// app.use(express.urlencoded());
+// app.use(express.static('./public'));
 
---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req,res){
+  req.setEncoding(<h1>Test this app</h1>)
+});
+
+module.exports = router;
